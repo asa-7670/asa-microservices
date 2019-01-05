1fe7f08c3f0d56013ef40759a7e9f4425a067497
abdelhalim.saidi <abdelhalim.saidi@sis-marches.com>
2019-01-06 00:15:53 +0100

    init & config eurika-server

--- client-ui/src/main/java/com/asa/app/microservices/client/ClientUiApplication.java
+++ client-ui/src/main/java/com/asa/app/microservices/client/ClientUiApplication.java
@@ -2,8 +2,10 @@ package com.asa.app.microservices.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ClientUiApplication {
 
 	public static void main(String[] args) {
--- mpaiement/src/main/java/com/asa/app/microservices/paiement/PaiementApplication.java
+++ mpaiement/src/main/java/com/asa/app/microservices/paiement/PaiementApplication.java
@@ -2,8 +2,10 @@ package com.asa.app.microservices.paiement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PaiementApplication {
 
 	public static void main(String[] args) {
