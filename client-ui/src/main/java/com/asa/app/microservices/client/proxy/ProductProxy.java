57b5d6a747fbe4d126355c26efd5a2b9dcc855ba
abdelhalim.saidi <abdelhalim.saidi@sis-marches.com>
2019-01-11 10:22:49 +0100

    init & config ribbon on client-ui

--- client-ui/src/main/java/com/asa/app/microservices/client/proxy/ProductProxy.java
+++ client-ui/src/main/java/com/asa/app/microservices/client/proxy/ProductProxy.java
@@ -1,6 +1,7 @@
 package com.asa.app.microservices.client.proxy;
 
 import com.asa.app.microservice.core.model.impl.ProductImpl;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,7 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 import java.util.UUID;
 
-@FeignClient(name = "asa-microservice-produits", url = "localhost:9010")
+@FeignClient(name = "asa-microservice-produits")
+@RibbonClient(name = "asa-microservice-produits")
 public interface ProductProxy {
 
     @GetMapping(value = "asaProduits/Produits")
